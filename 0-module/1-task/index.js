1a7dce52b0b5ac13bcf7c58cf5f544d457a4e62b
Mariana Salmassi <mariana.salmassi@gmail.com>
2023-02-05 20:13:17 +0300

    please review

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,3 +1,13 @@
-function sum(m, n) {
-  // ваш код...
-}
+let a = 1;
+let b = 1;
+let sum;
+
+sum = a + b;
+console.log(sum);
+
+let f = 2;
+let v = 5;
+let sum1;
+
+sum = v + f;
+console.log(sum1);
