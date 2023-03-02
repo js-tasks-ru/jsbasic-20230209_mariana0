dd51355e721da3193d27f75acf9d5cf6c94e1c1d
Mariana Salmassi <mariana.salmassi@gmail.com>
2023-03-02 20:54:45 +0300

    please review module 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,18 @@
 function toggleText() {
-  // ваш код...
+  const btn = document.querySelector(".toggle-text-button");
+  const content = document.querySelector(".text");
+
+  btn.addEventListener("click", btnClick);
+
+  function btnClick() {
+    console.log(content.classList);
+
+    if (content.classList.contains("hidden")) {
+      btn.textContent = "Скрыть элемент";
+    } else {
+      btn.textContent = "Показать элемент";
+    }
+
+    content.classList.toggle("hidden");
+  }
 }
