6933b6bc186a51925679bcf61cf0b14b9356872c
Mariana Salmassi <mariana.salmassi@gmail.com>
2023-03-04 19:00:02 +0300

    please check, error is still in code

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,18 +1,9 @@
 function toggleText() {
-  const btn = document.querySelector(".toggle-text-button");
-  const content = document.querySelector(".text");
-
-  btn.addEventListener("click", btnClick);
-
-  function btnClick() {
-    console.log(content.classList);
-
-    if (content.classList.contains("hidden")) {
-      btn.textContent = "Скрыть элемент";
-    } else {
-      btn.textContent = "Показать элемент";
-    }
-
-    content.classList.toggle("hidden");
-  }
+  window.onload = function() {
+    btn = document.querySelector('.toggle-text-button');
+    text = document.querySelector('#text');
+    btn.onclick = function() {
+      text.classList.toggle('hidden');
+    };
+  };
 }
